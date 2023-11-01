b2676992906d0106c775a27e6af51d536b6764e2
Martin Hristov <61838617+Asdiel85@users.noreply.github.com>
2023-11-01 16:26:45 +0200

    added more css on post componnent

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,4 +1,5 @@
 import styles from './Post.module.css'
+import heart from '../img/heart.svg'
 export default function Post () {
     return (
         <article className={styles.card}>
@@ -7,7 +8,9 @@ export default function Post () {
           <span>Post Name</span>
         </div>
         <img src='https://media.npr.org/assets/img/2017/09/12/macaca_nigra_self-portrait-3e0070aa19a7fe36e802253048411a38f14a79f8-s1100-c50.jpg' alt="" className={styles.postImg} />
-       <div className={styles.interaction}></div>
+       <div className={styles.interaction}>
+        <img className={styles.cardIcon} src={heart} alt="" />
+       </div>
         </article>
     )
 }
