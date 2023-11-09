1e87160a98bd3d1390ddb0e95b58d21405a345b6
Martin Hristov <61838617+Asdiel85@users.noreply.github.com>
2023-11-09 13:26:26 +0200

    added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Navigation from "./components/Navigation.jsx"
 import CreatePost from "./components/CreatePost.jsx"
 import styles from  './App.module.css'
+import Footer from "./components/Footer.jsx"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Navigation /> 
     <div className={styles.container}>
   <CreatePost />
+  <Footer />
     </div>
     </>
   )
