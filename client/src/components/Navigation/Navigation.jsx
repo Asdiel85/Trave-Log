5a58d213a98d09b269fa2e9509424b63065fb809
Martin Hristov <61838617+Asdiel85@users.noreply.github.com>
2023-11-13 13:49:27 +0200

    added user avatar component

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -2,24 +2,30 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import UserAvatar from '../UserAvatar/UserAvatar.jsx';
 import { Link } from 'react-router-dom';
 
 export default function Navigation() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand as={Link} to={'/'}>Travel Log</Navbar.Brand>
+        <Navbar.Brand as={Link} to={'/'}>
+          Travel Log
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-          </Nav>
+          <Nav className="me-auto"></Nav>
           <Nav>
-            <Nav.Link as={Link} to={'/login'}>Login</Nav.Link>
+            <Nav.Link as={Link} to={'/login'}>
+              Login
+            </Nav.Link>
             <Nav.Link as={Link} to={'/register'}>
               Register
             </Nav.Link>
-            <NavDropdown title="Dropdown2" id="collapsible-nav-dropdown">
-              <NavDropdown.Item as={Link} to={'/create'}>Create Post</NavDropdown.Item>
+            <NavDropdown title ={<UserAvatar userAvatar = {"https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D"}/>} id="collapsible-nav-dropdown">
+              <NavDropdown.Item as={Link} to={'/create'}>
+                Create Post
+              </NavDropdown.Item>
               <NavDropdown.Item as={Link} to={'/logout'}>
                 Logout
               </NavDropdown.Item>
--- client/src/components/PostCard/PostCard.jsx
+++ client/src/components/PostCard/PostCard.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom'
 import styles from './PostCard.module.css'
 import heart from '../../img/heart.svg'
+import UserAvatar from '../UserAvatar/UserAvatar.jsx'
 export default function Post ({userImg, country, imageUrl, _id}) {
     return (
         <article className={styles.card}>
         <div className={styles.info}>
-          <img src={userImg} alt="" className={styles.userImg} />
+          <UserAvatar userAvatar={"https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D"} />
           <span>{country}</span>
         </div>
         <Link to={`/post-details/${_id}`}>
