83506696a1b9dcb43956202e0ec8916a1680a9cb
Martin Hristov <61838617+Asdiel85@users.noreply.github.com>
2023-11-13 13:13:26 +0200

    added some responsive design

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,51 +1,38 @@
-import { Link } from 'react-router-dom';
-import styles from './Navigation.module.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-export default function Navigation() {
+function CollapsibleExample() {
   return (
-    <nav className={styles.navigation}>
-      <ul className={styles.links}>
-          <Link to={'/'}>
-            <div className={styles.title}>Travel Log</div>
-          </Link>
-        <div className={styles.icons}>
-          <Link to={'/'}>
-            <li>
-              <span>Dashboard</span>
-            </li>
-          </Link>
-          <Link to={'/create'}>
-            <li>
-              <span>Create Post</span>
-            </li>
-          </Link>
-          <Link>
-            <li>
-              <span>Profile</span>
-            </li>
-          </Link>
-          <Link to={'/login'}>
-            <li>
-              <span>Login</span>
-            </li>
-          </Link>
-          <Link to={'/logout'}>
-            <li>
-              <span>Logout</span>
-            </li>
-          </Link>
-          <Link to={'/register'}>
-            <li>
-              <span>Register</span>
-            </li>
-          </Link>
-          <li>
-            <Link>
-              <span>account_circle</span>
-            </Link>
-          </li>
-        </div>
-      </ul>
-    </nav>
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Travel Log</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
+
+export default CollapsibleExample;
