6124662e055a24e0de81791a0b9c9e67f718ab93
Martin Hristov <61838617+Asdiel85@users.noreply.github.com>
2023-11-13 12:57:39 +0200

    added routing after login and register

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -14,7 +14,7 @@ router.post('/register', async (req, res) => {
       isAdmin: false,
     });
 
-    res.status(200)
+    res.status(200).json('User registered')
   } catch (error) {
     res.status(400).json(error.message);
   }
