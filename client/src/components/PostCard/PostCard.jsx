ea49a58ebbe042669cdb301d0d023444966f44d5
Martin Hristov <mghristov85@gmail.com>
2023-11-15 21:42:38 +0200

    added interaction component

--- client/src/components/PostCard/PostCard.jsx
+++ client/src/components/PostCard/PostCard.jsx
@@ -5,6 +5,7 @@ import styles from './PostCard.module.css';
 import heart from '../../img/heart.svg';
 import UserAvatar from '../UserAvatar/UserAvatar.jsx';
 import EditDeleteBtns from '../EditDeleteBtns/EditDeleteBtns.jsx';
+import Interaction from '../Interaction/Interaction.jsx';
 export default function Post({ userAvatar, country, imageUrl, _id, owner }) {
   const [loggedUser, setLoggedUser] = useContext(UserContext);
   return (
@@ -16,19 +17,7 @@ export default function Post({ userAvatar, country, imageUrl, _id, owner }) {
       <Link to={`/post-details/${_id}`}>
         <img src={imageUrl} alt="Post image" className={styles.postImg} />
       </Link>
-      {loggedUser ? (
-        <div className={styles.interaction}>
-          <div>
-          <span>0</span>
-          {loggedUser.id !== owner.toString() ? (
-            <>
-              <img className={styles.cardIcon} src={heart} alt="Heart" />
-            </>
-          ) : null}
-          </div>
-          {loggedUser.id === owner ? <EditDeleteBtns /> : null}
-        </div>
-      ) : null}
+     <Interaction id={owner} />
     </article>
   );
 }
