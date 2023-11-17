261c6df84c3c7f09f8ffb393e3bf3b7494edc8e9
Martin Hristov <61838617+Asdiel85@users.noreply.github.com>
2023-11-17 13:01:04 +0200

    added delete post into user profile

--- client/src/components/Feed/Feed.jsx
+++ client/src/components/Feed/Feed.jsx
@@ -1,9 +1,12 @@
 import PostCard from '../PostCard/PostCard';
 import * as postService from '../../service/postService';
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Feed(confirmTask) {
   const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
+
   useEffect(() => {
     postService
       .getAllPosts()
@@ -11,15 +14,23 @@ export default function Feed(confirmTask) {
       .catch((err) => console.log(err));
   }, []);
 
-  const deletePost = async (id) =>  {
-    await postService.deletePost(id)
-    setPosts(posts.filter(post => post._id !== id))
-  }
+  const deletePost = async (id) => {
+    try {
+      await postService.deletePost(id);
+      setPosts(posts.filter((post) => post._id !== id));
+    } catch (error) {
+      navigate('*')
+    }
+  };
 
   return (
     <>
       {posts.map((post) => (
-        <PostCard key={post._id} {...post} confirmTask={() => deletePost(post._id)} />
+        <PostCard
+          key={post._id}
+          {...post}
+          confirmTask={() => deletePost(post._id)}
+        />
       ))}
     </>
   );
--- client/src/components/UserPosts/UserPosts.jsx
+++ client/src/components/UserPosts/UserPosts.jsx
@@ -1,12 +1,22 @@
 import * as userService from '../../service/userService.js';
+import * as postsService from '../../service/postService.js'
 import {handleResponse} from '../../utils/handleResponse.js';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PostCard from '../PostCard/PostCard.jsx';
 
-export default function UserPosts(id) {
+export default function UserPosts(id, confirmTask) {
   const [posts, setPosts] = useState([]);
   const navigate = useNavigate();
+
+  const deletePost = async (id) => {
+    try {
+     await postsService.deletePost(id)
+      setPosts(posts => posts.filter(post => post._id !== id))
+    } catch (error) {
+      navigate('*')
+    }
+  }
 
   useEffect(() => {
     userService
@@ -19,9 +29,9 @@ export default function UserPosts(id) {
   }, [id]);
   return (
     <>
-      {posts.map((post) => (
-        <PostCard key={post._id} {...post} />
-      ))}
+      {posts.length > 0 ? posts.map((post) => (
+        <PostCard key={post._id} {...post} confirmTask={() => deletePost(post._id)} />
+      ) ): <h2 style={{textAlign: 'center'}}>User has no posts</h2>}
     </>
   );
 }
