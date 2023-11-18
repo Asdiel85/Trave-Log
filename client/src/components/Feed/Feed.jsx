75f08ce21f402c1b7e6535a0f7ae5d1b38c938df
Martin Hristov <61838617+Asdiel85@users.noreply.github.com>
2023-11-18 11:43:39 +0200

    added loading spinner

--- client/src/components/Feed/Feed.jsx
+++ client/src/components/Feed/Feed.jsx
@@ -3,16 +3,21 @@ import * as postService from '../../service/postService';
 import { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../../contexts/AuthContext.js';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner.jsx';
 
 export default function Feed() {
+  const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState([]);
-  const [loggedUser, setLoggedUser] = useContext(UserContext)
+  const [loggedUser, setLoggedUser] = useContext(UserContext);
   const navigate = useNavigate();
 
   useEffect(() => {
     postService
       .getAllPosts()
-      .then((result) => setPosts(result))
+      .then((result) => {
+        setPosts(result);
+        setLoading(false);
+      })
       .catch((err) => navigate('*'));
   }, []);
 
@@ -21,20 +26,26 @@ export default function Feed() {
       await postService.deletePost(id);
       setPosts(posts.filter((post) => post._id !== id));
     } catch (error) {
-      navigate('*')
+      navigate('*');
     }
   };
 
   return (
     <>
-      {posts.map((post) => (
-        <PostCard
-          key={post._id}
-          {...post}
-          liked={post.likes.includes(loggedUser?.id)}
-          confirmTask={() => deletePost(post._id)}
-        />
-      ))}
+      {loading ? (
+        <LoadingSpinner />
+      ) : posts.length > 0 ? (
+        posts.map((post) => (
+          <PostCard
+            key={post._id}
+            {...post}
+            liked={post.likes.includes(loggedUser?.id)}
+            confirmTask={() => deletePost(post._id)}
+          />
+        ))
+      ) : (
+        <h1>There are no posts yet</h1>
+      )}
     </>
   );
 }
--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -7,30 +7,32 @@ import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import UserAvatar from '../UserAvatar/UserAvatar.jsx';
 import EditDeleteBtns from '../EditDeleteBtns/EditDeleteBtns.jsx';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner.jsx';
+import { handleResponse } from '../../utils/handleResponse.js';
 
 export default function PostDetails() {
   const { id } = useParams();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
   const [post, setPost] = useState({});
   const [loggedUser, setLoggedUser] = useContext(UserContext);
 
   useEffect(() => {
     postService
       .getPostDetails(id)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error('Not found');
-        }
-
-        return res.json();
+      .then((res) => handleResponse(res))
+      .then((post) => {
+        setPost(post);
+        setLoading(false)
       })
-      .then(setPost)
       .catch((err) => {
         navigate('*');
       });
   }, [id]);
 
   return (
+    <>
+    {loading ? <LoadingSpinner /> :
     <Card style={{ width: '60%', margin: '30px auto' }}>
       <Card.Img variant="top" src={post.imageUrl} />
       <Card.Body>
@@ -47,9 +49,12 @@ export default function PostDetails() {
         <ListGroup.Item> {post.description}</ListGroup.Item>
       </ListGroup>
       <Card.Body>
-        {loggedUser &&
-          loggedUser.id === post.owner ? <EditDeleteBtns id={post.owner} /> : null}
+        {loggedUser && loggedUser.id === post.owner ? (
+          <EditDeleteBtns id={post.owner} />
+        ) : null}
       </Card.Body>
     </Card>
+        }
+        </>
   );
 }
