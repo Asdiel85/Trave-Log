51fea91e851899ca39187cecfba3e8c953e68779
Martin Hristov <mghristov85@gmail.com>
2023-11-20 22:15:02 +0200

    added useForm hook into edit post component

--- client/src/hooks/useForm.jsx
+++ client/src/hooks/useForm.jsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
+import { validateValues } from '../utils/validatePostForm';
 
 export default function useForm(initialValues) {
     const [formValues, setFormValues] = useState(initialValues)
-
-    const onChangeHandler = (e) => {
+    const [errors, setErrors] = useState({});
+    const [submitting, setSubmitting] = useState(false);
+   
+  const onChangeHandler = (e) => {
         setFormValues(state => ({...state, [e.target.name]: e.target.value}))
     }
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setErrors(validateValues(formValues));
+        setSubmitting(true);
+      };
+
     return {
         formValues,
-        onChangeHandler
+        onChangeHandler,
+        handleSubmit,
+        errors,
+        submitting
     }
 }
