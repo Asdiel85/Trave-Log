ffa0756e84bf511af64b02b2d627dbd0fa28f269
Martin Hristov <61838617+Asdiel85@users.noreply.github.com>
2023-11-20 11:09:25 +0200

    edit form created

--- client/src/components/CreatePost/CreatePost.jsx
+++ client/src/components/CreatePost/CreatePost.jsx
@@ -4,12 +4,13 @@ import SubmitBtn from '../SubmitBtn/SubmitBtn.jsx';
 import ErrorParagraph from '../ErrorParagraph/ErrorParagraph.jsx'; 
 import * as postService from '../../service/postService.js';
 import { handleResponse } from '../../utils/handleResponse.js';
+import useForm from '../../hooks/useForm.jsx';
+import { validateValues } from '../../utils/validatePostForm.js';
 import { useState, useEffect } from 'react';
 import {useNavigate} from 'react-router-dom'
 
 export default function CreatePost() {
-  const imagePattern = /^(http|https):\/\//;
-  const [inputFields, setInputFields] = useState({
+ const {formValues, onChangeHandler} = useForm({
     country: '',
     city: '',
     imageUrl: '',
@@ -22,41 +23,15 @@ export default function CreatePost() {
   const [apiError, setApiError] = useState('');
   const [submitting, setSubmitting] = useState(false);
 
-  const validateValues = (inputValues) => {
-    let errors = {};
-    if (!inputValues.country) {
-      errors.country = 'Country is required';
-    }
-    if (!inputValues.city) {
-      errors.city = 'City is required';
-    }  
-    if (!inputValues.imageUrl.match(imagePattern)) {
-      errors.imageUrl = 'Invalid image url';
-    }
-    if (Number(inputValues.cost < 1)) {
-      errors.cost = 'Cost should be a positive number';
-    }
-    if (!inputValues.description) {
-      errors.description = 'Description is required';
-    } else if(inputValues.description.length < 20 || inputValues.description.length > 200){
-      errors.description = 'Description should be between 20 and 200 characters'
-    }
-    return errors;
-  };
-
-  const handleChange = (e) => {
-    setInputFields({ ...inputFields, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    setErrors(validateValues(inputFields));
+    setErrors(validateValues(formValues));
     setSubmitting(true);
   };
 
   async function finnishSubmit() {
     try {
-      const response = await postService.createPost(inputFields);
+      const response = await postService.createPost(formValues);
       await handleResponse(response);
     } catch (error) {
       setApiError(error.message);
@@ -82,8 +57,8 @@ export default function CreatePost() {
           name="country"
           placeholder="Country"
           id="country"
-          value={inputFields.country}
-          onChange={handleChange}
+          value={formValues.country}
+          onChange={onChangeHandler}
           error = {errors.country}
         />
         {errors.country ? <ErrorParagraph message={errors.country} /> : null}
@@ -94,8 +69,8 @@ export default function CreatePost() {
           name="city"
           placeholder="City"
           id="city"
-          value={inputFields.city}
-          onChange={handleChange}
+          value={formValues.city}
+          onChange={onChangeHandler}
           error = {errors.city}
         />
         {errors.city ? <ErrorParagraph message={errors.city} /> : null}
@@ -106,8 +81,8 @@ export default function CreatePost() {
           name="imageUrl"
           placeholder="Image"
           id="imageUrl"
-          value={inputFields.imageUrl}
-          onChange={handleChange}
+          value={formValues.imageUrl}
+          onChange={onChangeHandler}
           error = {errors.imageUrl}
         />
         {errors.imageUrl ? <ErrorParagraph message={errors.imageUrl} /> : null}
@@ -118,8 +93,8 @@ export default function CreatePost() {
           name="cost"
           placeholder="0"
           id="cost"
-          value={inputFields.cost}
-          onChange={handleChange}
+          value={formValues.cost}
+          onChange={onChangeHandler}
           error = {errors.cost}
         />
         {errors.cost ? <ErrorParagraph message={errors.cost} /> : null}
@@ -130,8 +105,8 @@ export default function CreatePost() {
           id="description"
           cols="30"
           rows="10"
-          value={inputFields.description}
-          onChange={handleChange}
+          value={formValues.description}
+          onChange={onChangeHandler}
         ></textarea>
         {errors.description ? <ErrorParagraph message={errors.description} /> : null}
         <SubmitBtn name="Create" />
---
+++ client/src/components/EditPost/EditPost.jsx
@@ -0,0 +1,114 @@
+import styles from './EditPost.module.css';
+import InputField from '../InputField/InputField.jsx';
+import SubmitBtn from '../SubmitBtn/SubmitBtn.jsx';
+import ErrorParagraph from '../ErrorParagraph/ErrorParagraph.jsx';
+import * as postService from '../../service/postService.js';
+import { handleResponse } from '../../utils/handleResponse.js';
+import useForm from '../../hooks/useForm.jsx';
+import { validateValues } from '../../utils/validatePostForm.js';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function EditPost() {
+  const { formValues, onChangeHandler } = useForm({
+    country: '',
+    city: '',
+    imageUrl: '',
+    cost: 0,
+    description: '',
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors(validateValues(formValues));
+    setSubmitting(true);
+  };
+
+  async function finnishSubmit() {
+    try {
+      const response = await postService.createPost(formValues);
+      await handleResponse(response);
+    } catch (error) {
+      setApiError(error.message);
+    }
+  }
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState({});
+  const [apiError, setApiError] = useState('');
+  const [submitting, setSubmitting] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && submitting) {
+      finnishSubmit();
+      navigate('/');
+    }
+  }, [errors]);
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.login}>
+      <h2>Create Post</h2>
+      {apiError ? <ErrorParagraph message={apiError} /> : null}
+      <InputField
+        label="country"
+        title="Country"
+        type="text"
+        name="country"
+        placeholder="Country"
+        id="country"
+        value={formValues.country}
+        onChange={onChangeHandler}
+        error={errors.country}
+      />
+      {errors.country ? <ErrorParagraph message={errors.country} /> : null}
+      <InputField
+        label="city"
+        title="city"
+        type="text"
+        name="city"
+        placeholder="City"
+        id="city"
+        value={formValues.city}
+        onChange={onChangeHandler}
+        error={errors.city}
+      />
+      {errors.city ? <ErrorParagraph message={errors.city} /> : null}
+      <InputField
+        label="imageUrl"
+        title="Image"
+        type="text"
+        name="imageUrl"
+        placeholder="Image"
+        id="imageUrl"
+        value={formValues.imageUrl}
+        onChange={onChangeHandler}
+        error={errors.imageUrl}
+      />
+      {errors.imageUrl ? <ErrorParagraph message={errors.imageUrl} /> : null}
+      <InputField
+        label="cost"
+        title="Cost"
+        type="number"
+        name="cost"
+        placeholder="0"
+        id="cost"
+        value={formValues.cost}
+        onChange={onChangeHandler}
+        error={errors.cost}
+      />
+      {errors.cost ? <ErrorParagraph message={errors.cost} /> : null}
+      <label htmlFor="description">Description</label>
+      <textarea
+        className={errors.description ? styles.errorTextarea : styles.textarea}
+        name="description"
+        id="description"
+        cols="30"
+        rows="10"
+        value={formValues.description}
+        onChange={onChangeHandler}
+      ></textarea>
+      {errors.description ? (
+        <ErrorParagraph message={errors.description} />
+      ) : null}
+      <SubmitBtn name="Create" />
+    </form>
+  );
+}
