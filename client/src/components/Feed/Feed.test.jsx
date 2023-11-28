521e7592d15d0ed5cae95ebb3fa59a832496d60d
Martin Hristov <mghristov85@gmail.com>
2023-11-28 17:02:53 +0200

    working on unit testing

--- client/src/components/Feed/Feed.test.jsx
+++ client/src/components/Feed/Feed.test.jsx
@@ -1,45 +1,53 @@
-// import * as postService from '../../service/postService';
+import { describe, expect, it, vi } from "vitest";
+import * as postService from '../../service/postService'
+import { BASE_URL } from "../../utils/constants";
 
-// jest.mock('../../service/postService', () => ({
-//   getAllPosts: jest.fn(),
-// }));
+const dataArray = [
+    {
+      _id: "6557985c286ee71f5cbf1041",
+      userAvatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_lj6Uv0ddAQ8LHb…",
+      country: "Usa",
+      city: "Seatle",
+      imageUrl: "https://media.timeout.com/images/106030305/750/562/image.jpg",
+      cost: 5678,
+      description: "From its emerald parks to the endless views of Puget Sound, Seattle is…",
+      likes: [],
+      owner: "655795e0286ee71f5cbf1014",
+      createdAt: "2023-11-17T16:44:12.834+00:00",
+      updatedAt: "2023-11-24T09:25:12.767+00:00",
+      __v: 0
+    },
+    {
+      _id: "6564d2a82834ac660cf7c947",
+      userAvatar: "https://wow.zamimg.com/modelviewer/live/webthumbs/outfit/127/298111.jp…",
+      country: "UsaEdit",
+      city: "Boston",
+      imageUrl: "https://media.timeout.com/images/105483065/750/562/image.jpg",
+      cost: 2222,
+      description: "dadasdasdasdasdsddsadas",
+      likes: [],
+      owner: "65579551286ee71f5cbf0fff",
+      createdAt: "2023-11-27T17:32:24.109+00:00",
+      updatedAt: "2023-11-28T12:50:34.458+00:00",
+      __v: 0
+    },
+  ];
+  
 
-// let data;
+describe('Tesing feed component', () => {
+    it('should render all the data', async () => {
+        global.fetch = vi.fn()
 
-// describe('Testing feed component', () => {
-//   it('should return posts', async () => {
-//     // Your mock data
-//     const mockData = [
-//       {
-//         "_id": {
-//           "$oid": "65579776286ee71f5cbf1021"
-//         },
-//         "userAvatar": "https://i.ytimg.com/vi/IQ5TQn5gE7o/maxresdefault.jpg",
-//         "country": "Usa",
-//         "city": "Portland",
-//         "imageUrl": "https://media.timeout.com/images/105937857/750/562/image.jpg",
-//         "cost": 6983,
-//         "description": "Portland is known for many things: its eccentric culture, its incredibly creative restaurants its theater and arts scene, and its outdoor beauty top the list of reasons to visit.",
-//         "likes": [],
-//         "owner": {
-//           "$oid": "65579551286ee71f5cbf0fff"
-//         },
-//         "createdAt": {
-//           "$date": "2023-11-17T16:40:22.748Z"
-//         },
-//         "updatedAt": {
-//           "$date": "2023-11-17T16:40:22.748Z"
-//         },
-//         "__v": 0
-//       },
-//     ];
+function createFetchResponse(data) {
+  return { json: () => new Promise((resolve) => resolve(data)) }
+}
 
-//     data = { data: mockData };
+fetch.mockResolvedValue(createFetchResponse(dataArray))
 
-//     postService.getAllPosts.mockResolvedValue(data);
+const data = await (await postService.getAllPosts())
 
-//     const posts = await postService.getAllPosts();
+expect(fetch).toHaveBeenCalledWith(`${BASE_URL}`)
 
-//     expect(posts).toEqual(data);
-//   });
-// });
+expect(data).toStrictEqual(dataArray)
+    })
+})
