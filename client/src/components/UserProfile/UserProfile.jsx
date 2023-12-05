9da492f6d14328f0c26550fc22b69943ed657671
Martin Hristov <mghristov85@gmail.com>
2023-12-05 14:29:23 +0200

    added user profile reducer

--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -1,6 +1,6 @@
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useContext } from 'react';
 import { UserContext } from '../../contexts/AuthContext.jsx';
@@ -11,19 +11,18 @@ import UserPosts from '../UserPosts/UserPosts.jsx';
 import EditDeleteBtns from '../EditDeleteBtns/EditDeleteBtns.jsx';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner.jsx';
 import { ErrorContext } from '../../contexts/ErrorContext.jsx';
+import { INITIAL_STATE, userProfileReducer } from '../../reducers/userProfileReducer.js';
 
 export default function UserProfile() {
   const { id } = useParams();
   const navigate = useNavigate()
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState({});
-  const [showPosts, setShowPosts] = useState(false);
+  const [state, dispatch] = useReducer(userProfileReducer, INITIAL_STATE)
   const [loggedUser, setLoggedUser] = useContext(UserContext);
   const [, setErrorMessage] = useContext(ErrorContext)
 
   const hadnleShowPostsClick = (e) => {
     e.preventDefault();
-    setShowPosts(!showPosts);
+    dispatch({type: "HANDLE_POSTS", payload: state.showPosts})
   };
 
   const deleteUser = async (id) => {
@@ -46,8 +45,7 @@ export default function UserProfile() {
       .getOne(id)
       .then((response) => handleResponse(response))
       .then((user) => {
-        setUser(user);
-        setLoading(false);
+        dispatch({type: "FETCH_USER", payload: user})
       })
       .catch((error) => {
         setErrorMessage(error.message)
@@ -57,28 +55,28 @@ export default function UserProfile() {
 
   return (
     <>
-      {loading ? (
+      {state.loading ? (
         <LoadingSpinner />
       ) : (
         <Card data-testid = 'card' style={{ maxWidth: '500px', width: '100%', margin: '30px auto' }}>
-          <Card.Img variant="top" src={user.userAvatar} />
+          <Card.Img variant="top" src={state.user.userAvatar} />
           <Card.Body>
             <Card.Title>
-              {user.firstName} {user.lastName}
+              {state.user.firstName} {state.user.lastName}
             </Card.Title>
           </Card.Body>
           <ListGroup className="list-group-flush">
-            <ListGroup.Item>Email: {user.email}</ListGroup.Item>
+            <ListGroup.Item>Email: {state.user.email}</ListGroup.Item>
             <ListGroup.Item data-testid = 'showPostsBtn' as={Button} onClick={hadnleShowPostsClick}>
               Show Posts
             </ListGroup.Item>
           </ListGroup>
-          {loggedUser && loggedUser.id === user._id ? (
-            <EditDeleteBtns id={user._id} item= "user" confirmTask={() =>deleteUser(user._id)}/>
+          {loggedUser && loggedUser.id === state.user._id ? (
+            <EditDeleteBtns id={state.user._id} item= "user" confirmTask={() =>deleteUser(state.user._id)}/>
           ) : null}
         </Card>
       )}
-      {showPosts && <UserPosts id={user._id} />}
+      {state.showPosts && <UserPosts id={state.user._id} />}
     </>
   );
 }
---
+++ client/src/reducers/userProfileReducer.js
@@ -0,0 +1,23 @@
+export const INITIAL_STATE = {
+  loading: true,
+  user: {},
+  showPosts: false,
+};
+
+export const userProfileReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_USER":
+      return {
+        ...state,
+        loading: false,
+        user: action.payload,
+        showPosts: false
+      };
+    case "HANDLE_POSTS":
+      return {
+        ...state,
+        loading: false,
+        showPosts: !action.payload,
+      };
+  }
+};
